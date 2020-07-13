70d4ef03c3caa6685bf89c885032533337c126f3
Golovina, Yevgeniya <Yevgeniya.Golovina@nexign.com>
2020-07-13 18:14:56 +0300

    chart

--- src/pages/Scheme/components/DistributionChart/DistributionChart.tsx
+++ src/pages/Scheme/components/DistributionChart/DistributionChart.tsx
@@ -1,8 +1,7 @@
-//@ts-ignore
 import React, { useRef, useEffect } from 'react'
 import * as d3 from 'd3'
 import style from './style.module.css'
-import { Numeric } from 'd3'
+import './DistributionChart.css'
 
 const height = 300
 const width = 700
@@ -27,53 +26,158 @@ interface Data {
     p: number
 }
 const array1 = Random_normal_Dist(30, 15) as Data[]
-const array2 = Random_normal_Dist(30, 10) as Data[]
+const array2 = Random_normal_Dist(-16, 22) as Data[]
 
 const DistributionChart: React.FC<{}> = () => {
     const d3Container = useRef(null)
 
     useEffect(() => {
         if (d3Container.current) {
+            const margin = { top: 20, right: 30, bottom: 30, left: 40 }
+            const data = array1
+            const data2 = array2
             const svg = d3.select(d3Container.current)
+
+            svg.selectAll('*').remove()
+
             const getX = (d: Data) => d.q
             const getY = (d: Data) => d.p
-            const x = d3.scaleLinear().rangeRound([0, width])
-            // .domain([d3.min(points, getX), d3.max(points, getX)])
-            // min q
-            let d1 = d3.min(array1, getX) as Numeric
-            let d2 = d3.min(array1, getX) as Numeric
-            const min_d = d3.min([d1, d2]) as Numeric
-            // max q
-            d1 = d3.max(array1, getX) as Numeric
-            d2 = d3.max(array1, getX) as Numeric
-            const max_d = d3.max([d1, d2]) as Numeric
-            // max p
-            d1 = d3.max(array1, getY) as Numeric
-            d2 = d3.max(array1, getY) as Numeric
-            const max_p = d3.max([d1, d2]) as Numeric
-            const y = d3.scaleLinear().domain([0, max_p]).range([height, 0])
-            x.domain([min_d, max_d]).nice()
-            const chart = d3
-                .select(d3Container.current)
-                .append('svg')
+
+            const x = d3
+                .scaleLinear()
+                .domain([
+                    d3.min(data, getX) as number,
+                    d3.max(data, getX) as number,
+                ])
+                .range([margin.left, width - margin.right])
+
+            const y = d3
+                .scaleLinear()
+                .domain([
+                    d3.min(data, getY) as number,
+                    d3.max(data, getY) as number,
+                ])
+                .range([height - margin.bottom, margin.top])
+
+            svg.append('svg').attr('width', 1440).attr('height', 30)
+
+            const xAxis = (g: any) =>
+                g
+                    .attr('transform', `translate(0,${height - margin.bottom})`)
+                    .attr('class', 'grid')
+                    .call(
+                        d3
+                            .axisBottom(x)
+                            .ticks(width / 50)
+                            .tickSize(-height)
+                            .tickSizeOuter(0)
+                            .tickFormat(() => '')
+                    )
+            const yAxis = (g: any) =>
+                g
+                    .attr('transform', `translate(${margin.left},0)`)
+                    .attr('class', 'grid')
+                    .call(
+                        d3
+                            .axisLeft(y)
+                            .ticks(height / 70)
+                            .tickSize(-width)
+                            .tickFormat(() => '')
+                    )
+                    .call((g: any) => g.select('.domain').remove())
+                    .call((g: any) =>
+                        g
+                            .select('.tick:last-of-type text')
+                            .clone()
+                            .attr('x', 3)
+                            .attr('text-anchor', 'start')
+                            .attr('font-weight', 'bold')
+                            // @ts-ignore
+                            .text(data.y)
+                    )
+            svg.append('g').call(xAxis)
+            svg.append('g').call(yAxis)
+
+            const area = d3
+                .area()
+                //@ts-ignore
+                // .defined((d) => !isNaN(d.upper))
+                .x((d: Data) => x(getX(d)))
+                .y0(height)
+                .y1((d: Data) => y(getY(d)))
+
+            const line = d3
+                .line()
+                //@ts-ignore
+                // .defined((d) => !isNaN(d.upper))
+                .x((d: Data) => x(getX(d)))
+                .y((d: Data) => y(getY(d)))
+
+            const graphArea = svg.append('g')
+            const defs = svg.append('defs')
+
+            const bgGradient = defs
+                .append('linearGradient')
+                .attr('id', `main-content-bg-gradient`)
+                .attr('gradientTransform', 'rotate(90)')
+
+            bgGradient
+                .append('stop')
+                .attr('stop-color', 'rgba(41, 176, 255, 0.4)')
+                .attr('offset', '0%')
+            bgGradient
+                .append('stop')
+                .attr('stop-color', 'rgba(41, 176, 255, 0)')
+                .attr('offset', '100%')
+
+            defs.append('clipPath')
+                .attr('id', `main-content-clip-line-path`)
+                .append('path')
+                .attr('d', area(data))
+                .attr('class', 'value-line')
+
+            const clipPath = graphArea
+                .append('g')
+                .attr('clip-path', `url(#main-content-clip-line-path)`)
+
+            clipPath
+                .append('rect')
+                .attr('x', 0)
+                .attr('y', 0)
                 .attr('width', width)
                 .attr('height', height)
-            const gX = chart
-                .append('g')
-                .attr('class', 'x axis')
-                .attr('transform', 'translate(0,' + height + ')')
-                .call(d3.axisBottom(x))
-            let line = d3
-                .line()
-                .x((d) => x(d.q))
-                .y((d) => y(d.p))
+                .style('fill', 'url(#main-content-bg-gradient)')
+
+            graphArea
+                .append('path')
+                .datum(data)
+                .attr('fill', 'none')
+                .attr('stroke', '#56B9F2')
+                .attr('stroke-width', 1)
+                .attr(
+                    'd',
+                    d3
+                        .line()
+                        //@ts-ignore
+                        .x((d: Data) => x(getX(d)))
+                        .y((d: Data) => y(getY(d)))
+                )
+
+            svg.append('path')
+                .datum(data2)
+                .attr('fill', 'none')
+                .attr('stroke', '#fff')
+                .attr('stroke-width', 1.5)
+                .attr('stroke-linejoin', 'round')
+                .attr('stroke-linecap', 'round')
+                //@ts-ignore
+                .attr('d', line)
         }
     }, [])
 
     return (
         <div className={style.Chart}>
             <svg width={width} height={height} ref={d3Container} />
-            <div>chart</div>
         </div>
     )
 }
