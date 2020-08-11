19e8c9c7384dda1ff105cb15fa261ec33a540e8f
Golovina, Yevgeniya <Yevgeniya.Golovina@nexign.com>
2020-08-11 16:49:33 +0300

    Set up .env

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
-    uri: 'http://localhost:8000/graphql',
+    uri: `${process.env.REACT_APP_BACKEND_API}/graphql`,
     cache: new InMemoryCache(),
 })
 
