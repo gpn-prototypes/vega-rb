56e2680df10fa548ad4be83d0036265e5b842f8e
Golovina, Yevgeniya <Yevgeniya.Golovina@nexign.com>
2020-08-11 18:28:58 +0300

    Set up backend host api

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
-    uri: `${process.env.REACT_APP_BACKEND_API}/graphql`,
+    uri: '/graphql',
     cache: new InMemoryCache(),
 })
 
