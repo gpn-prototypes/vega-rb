fb5234d25f3ab513a6e799bbac1c2161ff9debfc
Kirill.Sadovnikov <kirill.sadovnikov@nexign.com>
2020-08-12 10:21:36 +0300

    Fix default table rows

--- src/utils/fakerGenerators.ts
+++ src/utils/fakerGenerators.ts
@@ -1,10 +1,10 @@
-import faker from 'faker'
+// import faker from 'faker'
 
-export const mockTableRows = [...Array(10)].map((_, index) => ({
+export const mockTableRows = [...Array(1000)].map((_, index) => ({
     id: index,
-    area: faker.random.arrayElement(['Надежденский', 'Угловой_Сц-4_base']),
-    deposit: faker.random.arrayElement(['РА-132', 'Незалёжное', 'Д122-УЩ']),
-    layer: faker.random.arrayElement(['Д 5', 'P50', 'Т1 + Т2']),
-    well: faker.random.number(20).toString(),
-    mine: faker.random.arrayElement(['FIR-2323-L', 'R-565', 'CC 89732-1']),
+    // area: faker.random.arrayElement(['Надежденский', 'Угловой_Сц-4_base']),
+    // deposit: faker.random.arrayElement(['РА-132', 'Незалёжное', 'Д122-УЩ']),
+    // layer: faker.random.arrayElement(['Д 5', 'P50', 'Т1 + Т2']),
+    // well: faker.random.number(20).toString(),
+    // mine: faker.random.arrayElement(['FIR-2323-L', 'R-565', 'CC 89732-1']),
 }))
