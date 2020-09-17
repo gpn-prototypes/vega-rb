806df9541dc8a8105c9820d745cef994112bd12b
Kirill.Sadovnikov <kirill.sadovnikov@nexign.com>
2020-09-17 17:23:11 +0300

    fix(global): proxy endpoint has been fixed

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function module(app) {
   if (process.env.NODE_ENV === 'development' && process.env.REACT_APP_PROXY) {
     app.use(
-      '/',
+      '/graphql',
       createProxyMiddleware({
         target: process.env.REACT_APP_PROXY,
         changeOrigin: true,
