31eb4c9b52306c56801b90be634d55e734e09b55
Rodion Lunev <ronikjkee@yandex.ru>
2020-11-13 19:58:44 +0300

    fix(build): исправил ошибку при сборке.

--- src/pages/Scheme/helpers.ts
+++ src/pages/Scheme/helpers.ts
@@ -1,12 +1,12 @@
-import { Conception } from 'generated/graphql';
+import { Conception, RbProjectInput } from 'generated/graphql';
 
 export const getGraphqlUri = (projectId: string): string =>
   `http://localhost:8080/graphql/${projectId}`;
 
-export const getDownloadResultUri = (id: string) =>
+export const getDownloadResultUri = (id: string): string =>
   `http://localhost:8080/files/calculation_result/${id}`;
 
-export const getMockConceptions = (conception: Conception) => {
+export const getMockConceptions = (conception: Conception): RbProjectInput => {
   return {
     version: '0.1.0',
     conceptions: [conception],
