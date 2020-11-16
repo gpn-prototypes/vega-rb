472995742776a81bd09769c04ef335e73389a810
Rodion Lunev <ronikjkee@yandex.ru>
2020-11-16 12:53:39 +0300

    fix(build): исправил ссылку на API

--- config/env.js
+++ config/env.js
@@ -62,30 +62,22 @@ const REACT_APP = /^REACT_APP_/i;
 
 function getClientEnvironment(publicUrl) {
   const raw = Object.keys(process.env)
-    .filter(key => REACT_APP.test(key))
     .reduce(
       (env, key) => {
         env[key] = process.env[key];
         return env;
       },
       {
+
         APP_MODE: process.env.APP_MODE || 'standalone',
-        // Useful for determining whether we’re running in production mode.
-        // Most importantly, it switches React into the correct mode.
         NODE_ENV: process.env.NODE_ENV || 'development',
-        // Useful for resolving the correct path to static assets in `public`.
-        // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
-        // This should only be used as an escape hatch. Normally you would put
-        // images into the `src` and `import` them in code to get their paths.
         PUBLIC_URL: publicUrl,
-        // We support configuring the sockjs pathname during development.
-        // These settings let a developer run multiple simultaneous projects.
-        // They are used as the connection `hostname`, `pathname` and `port`
-        // in webpackHotDevClient. They are used as the `sockHost`, `sockPath`
-        // and `sockPort` options in webpack-dev-server.
         WDS_SOCKET_HOST: process.env.WDS_SOCKET_HOST,
         WDS_SOCKET_PATH: process.env.WDS_SOCKET_PATH,
         WDS_SOCKET_PORT: process.env.WDS_SOCKET_PORT,
+        BASE_API_URL: process.env.BASE_API_URL || 'http://outsourcing.nat.tepkom.ru:38080',
+        API_PATH: process.env.API_PATH || '/',
+        YC_DEPLOYMENT: process.env.YC_DEPLOYMENT === 'true'
       }
     );
   // Stringify all values so we can feed into webpack DefinePlugin
--- src/pages/Scheme/helpers.ts
+++ src/pages/Scheme/helpers.ts
@@ -1,10 +1,10 @@
 import { Conception, RbProjectInput } from 'generated/graphql';
 
 export const getGraphqlUri = (projectId: string): string =>
-  `http://localhost:8080/graphql/${projectId}`;
+  `${process.env.BASE_API_URL}/graphql/${projectId}`;
 
 export const getDownloadResultUri = (id: string): string =>
-  `http://localhost:8080/files/calculation_result/${id}`;
+  `${process.env.BASE_API_URL}/files/calculation_result/${id}`;
 
 export const getMockConceptions = (conception: Conception): RbProjectInput => {
   return {
