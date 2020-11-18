2d594fd60bb8192ca42fd1ad491200922c87f7a7
Rodion Lunev <ronikjkee@yandex.ru>
2020-11-18 14:32:30 +0300

    fix(columns): - Добавил контекстное меню для колонки расчетных параметров

--- src/components/ExcelTable/Header/HeaderWithContextMenu.tsx
+++ src/components/ExcelTable/Header/HeaderWithContextMenu.tsx
@@ -1,16 +1,22 @@
 import React, { ReactElement } from 'react';
 import { withContextMenu } from 'components/ExcelTable/ContextMenu';
 import {
+  CALCULATION_PARAMS_HEADER_CONTEXT_ID,
   HEADER_CONTEXT_ID,
   HeaderRendererProps,
+  TableEntities,
 } from 'components/ExcelTable/types';
 
 import Header from './Header';
 
 export const HeaderWithContextMenu = (
   props: HeaderRendererProps,
-): ReactElement =>
-  withContextMenu(<Header {...props} />, {
-    id: HEADER_CONTEXT_ID,
+): ReactElement => {
+  return withContextMenu(<Header {...props} />, {
+    id:
+      props.column.type === TableEntities.CALC_PARAM
+        ? CALCULATION_PARAMS_HEADER_CONTEXT_ID
+        : HEADER_CONTEXT_ID,
     collect: () => ({ idx: props.column.idx }),
   });
+};
